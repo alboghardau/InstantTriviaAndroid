8ab114e89e171424de579e93a8bc6fcda13780f9
albog <alboghardau@gmail.com>
2020-01-05 00:07:31 +0200

    User interface/service

--- app/src/main/java/com/abh/instanttrivia/services/UserService.java
+++ app/src/main/java/com/abh/instanttrivia/services/UserService.java
@@ -1,6 +1,22 @@
 package com.abh.instanttrivia.services;
 
-public class UserService {
+import com.abh.instanttrivia.interfaces.UserInterface;
+import com.abh.instanttrivia.model.User;
 
+public class UserService implements UserInterface {
 
+    @Override
+    public void registerUser(User user) {
+
+    }
+
+    @Override
+    public void loginUser(User user) {
+
+    }
+
+    @Override
+    public void checkToken(String token) {
+
+    }
 }
