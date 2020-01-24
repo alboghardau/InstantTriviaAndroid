18c5ba28fb239f3f7258785648f4da3e5705dca3
albog <alboghardau@gmail.com>
2020-01-24 14:58:27 +0200

    QuestionService.getRandomQuestion()

--- app/src/main/java/com/abh/instanttrivia/services/QuestionService.java
+++ app/src/main/java/com/abh/instanttrivia/services/QuestionService.java
@@ -1,45 +1,41 @@
 package com.abh.instanttrivia.services;
 
+import android.util.JsonReader;
 import android.util.Log;
 
 import com.abh.instanttrivia.interfaces.QuestionInterface;
+import com.abh.instanttrivia.model.Category;
 import com.abh.instanttrivia.model.Question;
+import com.abh.instanttrivia.network.WebPostAsync;
 
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
 
 public class QuestionService implements QuestionInterface {
 
-    private SendPostService sendPostService;
-
-    public QuestionService(SendPostService sendPostService) {
-        this.sendPostService = sendPostService;
-    }
-
-    @Override
+    //TESTED
     public Question getRandomQuestion() {
         try {
-            String data = this.sendPostService.execute("http://itrivia.eu/api/question/getRandomQuestion/").get();
-            Log.e("DATA", data);
-            JSONObject jsonObject = new JSONObject(data);
+            String data   = new WebPostAsync().execute("http://itrivia.eu/api/question/getRandomQuestion/", "").get();
 
+            JSONObject questionJson = new JSONObject(data);
             Question question = new Question();
-            question.setQuestion(jsonObject.getString("Question"));
+            question.setId(Integer.parseInt(questionJson.getString("Id")));
+            question.setQuestion(questionJson.getString("Question"));
+            question.setAnswer(questionJson.getString("Answer"));
+            question.setDifficulty(Double.parseDouble(questionJson.getString("Difficulty")));
 
-            Log.e("OBJ", question.toString());
+            JSONObject categoryJson = new JSONObject(questionJson.getString("Category"));
+            Category category = new Category(categoryJson.getInt("Id"),
+                    categoryJson.getString("Name"));
+
+            question.setCategory(category);
 
             return question;
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-            return null;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return null;
-        } catch (JSONException e) {
-            e.printStackTrace();
+        } catch (Exception e){
+            Log.e("Error", e.toString());
             return null;
         }
     }
+
+
 }
