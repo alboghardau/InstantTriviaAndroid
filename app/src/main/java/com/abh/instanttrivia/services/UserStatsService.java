47ca690a14ed3be1355f0a14e73751c1771a5456
albog <alboghardau@gmail.com>
2020-01-29 16:43:50 +0200

    UserStatService get

--- app/src/main/java/com/abh/instanttrivia/services/UserStatsService.java
+++ app/src/main/java/com/abh/instanttrivia/services/UserStatsService.java
@@ -1,6 +1,35 @@
 package com.abh.instanttrivia.services;
 
-public class UserStatsService {
+import com.abh.instanttrivia.interfaces.UserStatsInterface;
+import com.abh.instanttrivia.model.User;
+import com.abh.instanttrivia.model.UserStats;
+import com.abh.instanttrivia.network.WebPostAsync;
 
+import org.json.JSONObject;
 
+public class UserStatsService implements UserStatsInterface {
+
+    private SharedPrefService sharedPrefService;
+
+    public UserStatsService(SharedPrefService sharedPrefService){
+        this.sharedPrefService = sharedPrefService;
+    }
+
+    @Override
+    public UserStats getStats(User user) {
+        try{
+            String data = new WebPostAsync().execute("http://itrivia.eu/api/user/getStats/", user.toJSON()).get();
+            JSONObject jsonObject = new JSONObject(data);
+            UserStats userStats = new UserStats.Builder()
+                    .experience(jsonObject.getInt("Experience"))
+                    .level(jsonObject.getInt("Level"))
+                    .coins(jsonObject.getInt("Coins"))
+                    .played(jsonObject.getInt("Played"))
+                    .answered(jsonObject.getInt("Answered"))
+                    .build();
+            return userStats;
+        }catch (Exception e){
+            return null;
+        }
+    }
 }
